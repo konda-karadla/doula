8de15137217828d71116bc67a7d90696e82fcdf1
konda-karadla <konda.karadla@truebill.ai>
2025-10-03 21:04:17 +0530

    Deleted tsconfig.tsbuildinfo

--- dist/src/labs/services/ocr.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class OcrService {
-    private readonly logger;
-    extractTextFromBuffer(buffer: Buffer): Promise<string>;
-    extractTextFromFile(file: Express.Multer.File): Promise<string>;
-}
